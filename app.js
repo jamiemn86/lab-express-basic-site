0261adb81bf7630db1a0c410b97aaadd7a013086
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-08-13 13:10:38 +0200

    Three pages working, links added from home page

--- app.js
+++ app.js
@@ -17,5 +17,13 @@ app.get('/works', (request, response, next) =>
   response.sendFile(__dirname + '/views/works.html')
 );
 
+app.get('/stylesheets/style.ss', (request, response, next) =>
+  response.sendFile(__dirname + '/public/stylesheets/style.css')
+);
+
+app.get('*', (request, response, next) =>
+  response.sendFile(__dirname + '/views/home.html')
+);
+
 // Server Started
 app.listen(3000, () => console.log('My first app listening on port 3000!'));
